344b049ead208e5a0e2fc2ee50f7a6c5f8b0bf6a
Brian Haw <brianshiroe@gmail.com>
2026-04-09 00:08:16 +0400

    [add] page: new components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import { Hero } from "../components/Hero";
+import { About } from "@/components/About";
 import { Skills } from "../components/Skills";
 import { Projects } from "../components/Projects";
+import { Experience } from "@/components/Experience";
+import { CurriculumVitae } from "@/components/CurriculumVitae";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center bg-white">
       <Hero />
+      <About />
       <Skills />
       <Projects />
+      <Experience />
+      <CurriculumVitae />
     </div>
   );
 }
