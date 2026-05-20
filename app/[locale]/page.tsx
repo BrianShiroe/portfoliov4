aab72e9203e6bbb8bfe02483b90c9e6723123019
Brian Haw <brianshiroe@gmail.com>
2026-05-20 11:17:58 +0400

    [update] main: rearange section format

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -11,29 +11,18 @@ import { TechTicker } from '@/components/home/TechTicker';
 export default function Home() {
   return (
     <div className="flex flex-col items-center bg-white w-full">
-      {/* 1. Home Section */}
       <Hero />
+      <About />
       
       {/* Divider */}
       <TechTicker />
 
-      {/* 2. Projects Section (Includes both main and enterprise list) */}
       <Projects />
       <EnterpriseProjects />
       
-      {/* 3. About Section */}
-      <About />
-      
-      {/* 4. Skills Section (GSAP Pinned Horizontal Track) */}
       <Skills />
-      
-      {/* 5. Services Section */}
       <Services />
-      
-      {/* 6. Experience Section */}
       <Experience />
-      
-      {/* 7. Contact Section */}
       <Contact />
     </div>
   );
