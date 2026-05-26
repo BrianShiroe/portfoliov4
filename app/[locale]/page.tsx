f1b474c666d400388cb1681af7a94654ed6d8329
Brian Haw <brianshiroe@gmail.com>
2026-05-26 14:27:59 +0400

    [update] main: move techticker up

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,25 +1,25 @@
-import { Hero } from '@/components/home/Hero';
-import { About } from '@/components/home/About';
-import { Services } from '@/components/home/Services';
-import { Skills } from '@/components/home/Skills';
-import { TopProjects } from '@/components/home/TopProjects';
-import { AllProjects } from '@/components/home/AllProjects'; 
-import { Experience } from '@/components/home/Experience';
-import { Contact } from '@/components/home/Contact';
-import { TechTicker } from '@/components/home/TechTicker'; 
+import { Hero } from "@/components/home/Hero";
+import { About } from "@/components/home/About";
+import { Services } from "@/components/home/Services";
+import { Skills } from "@/components/home/Skills";
+import { TopProjects } from "@/components/home/TopProjects";
+import { AllProjects } from "@/components/home/AllProjects";
+import { Experience } from "@/components/home/Experience";
+import { Contact } from "@/components/home/Contact";
+import { TechTicker } from "@/components/home/TechTicker";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center bg-white w-full">
       <Hero />
-      <About />
-      
+
       {/* Divider */}
       <TechTicker />
+      <About />
 
       <TopProjects />
       <AllProjects />
-      
+
       <Skills />
       <Services />
       {/* <Experience /> */}
